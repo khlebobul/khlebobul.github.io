6c9e21ccd0b479459d9f7e4e9f46dcbe7930edaa
Gleb Shalimov <sbgleb10@gmail.com>
2024-05-20 14:42:25 +0300

    add google-site-verification

--- sketch.js
+++ sketch.js
@@ -9,10 +9,10 @@ function draw() {
     noStroke();
     rectMode(CENTER);
     let spacing = 20;
-    let noiseScale = map(mouseX, 0, width, 0.005, 0.02); // Можно варьировать масштаб шума для лучшей визуализации
+    let noiseScale = map(mouseX, 0, width, 0.005, 0.02);
     for (let y = 0; y < height; y += spacing) {
         for (let x = 0; x < width; x += spacing) {
-            let n = noise(x * noiseScale, y * noiseScale, frameCount * 0.01); // Изменяем частоту кадра для анимации
+            let n = noise(x * noiseScale, y * noiseScale, frameCount * 0.01);
             let letter;
             if (n > 0.6) {
                 letter = "█";
